29b9268632f8616531ad0b63649261c6e379d0dd
Héctor Sebastián Trejo Luna <trejolunatutoriales@gmail.com>
2023-01-20 00:47:33 +0000

    cleaned home and made backup

--- pages/indexblogbackup.js
+++ pages/indexblogbackup.js
@@ -6,6 +6,7 @@ import { getClient, usePreviewSubscription } from "@lib/sanity";
 import defaultOG from "../public/img/opengraph.jpg";
 import { postquery, configQuery } from "@lib/groq";
 import GetImage from "@utils/getImage";
+import PostList from "@components/postlist";
 
 export default function Post(props) {
   const { postdata, siteconfig, preview } = props;
@@ -54,9 +55,24 @@ export default function Post(props) {
           />
           <Container>
             <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
-              <h2>Hello</h2>
+              {posts.slice(0, 2).map(post => (
+                <PostList
+                  key={post._id}
+                  post={post}
+                  aspect="landscape"
+                  preloadImage={true}
+                />
+              ))}
             </div>
-  
+            <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-3 ">
+              {posts.slice(2).map(post => (
+                <PostList
+                  key={post._id}
+                  post={post}
+                  aspect="square"
+                />
+              ))}
+            </div>
           </Container>
         </Layout>
       )}
